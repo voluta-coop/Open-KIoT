51657f8ef8b8c1bc850d93dcbf76f2b1d2ba824f
voluta.coop <jalbalate@voluta.coop>
2022-06-27 14:34:08 +0200

    Corrección FW Test_Riego

--- FW/TEST_Riego/TEST_Riego.ino
+++ FW/TEST_Riego/TEST_Riego.ino
@@ -1,28 +1,7 @@
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>
-
-// Wifi
-#define WIFI_SSID   "XXXXXX"
-#define WIFI_PASSWORD   "XXXXXXXX"
-
-// Telegram
-#define BOT_TOKEN   "XXXXXXXXXXXXXXXX"
-#define CHAT_ID   "XXXXXXXXXXX"
-
 
 #define PIN_BOMBA 26
 #define PIN_BOYA 27
 #define PIN_HUM_TIERRA 33
-
-
-WiFiClientSecure secured_client;
-
-UniversalTelegramBot bot(BOT_TOKEN, secured_client);
-
-
-const unsigned long BOT_MTBS = 1000; // mean time between scan messages
-unsigned long bot_lasttime; // last time messages' scan has been done
 
 bool bomba = false;
 bool boya = false;
@@ -36,32 +15,13 @@ void setup()
 {
   Serial.begin(9600);
   
-  
-  // attempt to connect to Wifi network:
-  Serial.print("Conectando al Wifi ");
-  Serial.print(WIFI_SSID);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-
   pinMode(PIN_BOMBA, OUTPUT);
   pinMode(PIN_BOYA, INPUT);
   
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.print("\nWiFi conectado. Dirección IP: ");
-  Serial.println(WiFi.localIP());
 }
+
 void loop()
 {
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
 
   if(millis() - tiempo > 60000 || tiempo == 0 || bomba == true){
 
@@ -76,18 +36,15 @@ void loop()
     if(humedad_tierra_100 < 0) humedad_tierra_100 = 0;
     
     if(boya == false){
-      bot.sendMessage(CHAT_ID,"Debes rellenar el agua del riego", "");
       bomba = false;
     }
   
     else if(boya == true && humedad_tierra_100 < hum_min && bomba == false) {
-      bot.sendMessage(CHAT_ID,"Riego encendido", "");
       bomba = true;
     }
   
     else if(boya == true && humedad_tierra_100 > hum_max){
       bomba = false;
-      bot.sendMessage(CHAT_ID,"Riego apagado", "");
     }
 
     Serial.print("Humedad : ");
